ea94c601356585db8b52b951ac958fe01a5c24a9
Nelvin Ochieng <nelvinakinyi130@gmail.com>
2026-07-29 22:07:56 -0700

    Add first working database connection

---
+++ main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+
+	_ "modernc.org/sqlite"
+)
+
+func main() {
+	db, err := sql.Open("sqlite", "chama.db")
+	if err != nil {
+		log.Fatal("Failed to open database:", err)
+	}
+	defer db.Close()
+
+	err = db.Ping()
+	if err != nil {
+		log.Fatal("Failed to connect to database:", err)
+	}
+
+	fmt.Println("Successfully connected to chama.db!")
+}
